5c8204483eb13bffa7204bb8ac60e5ac47f1d58c
Idan <idan8ezer@gmail.com>
2022-04-23 21:13:26 +0300

    קבםמג בםצצןא

--- HeroesLeague/GameView.swift
+++ HeroesLeague/GameView.swift
@@ -17,50 +17,73 @@ class GameView: UIViewController {
     @IBOutlet weak var panel_IMG_card1: UIImageView!
     @IBOutlet weak var panel_IMG_card2: UIImageView!
     
-    var player1:String = "Player1"
-    var player2:String = "Player2"
-    var score1 : Int = 0
-    var score2 : Int = 0
+    var player1: String = "Player1"
+    var player2: String = "Player2"
+    var round: Int = 0
+    var score1: Int = 0
+    var score2: Int = 0
+    var timer = Timer();
     let images = [#imageLiteral(resourceName: "spider-man"), #imageLiteral(resourceName: "dr-octopus"), #imageLiteral(resourceName: "thor"), #imageLiteral(resourceName: "loki"), #imageLiteral(resourceName: "batman"), #imageLiteral(resourceName: "joker"), #imageLiteral(resourceName: "iron-man"), #imageLiteral(resourceName: "mandarin"), #imageLiteral(resourceName: "cap-america"), #imageLiteral(resourceName: "red-skull")]
 
     
     override func viewDidLoad() {
         self.view.backgroundColor = UIColor(red: 183/255, green: 183/255, blue: 164/255, alpha: 1)
         super.viewDidLoad()
-        startGame()
+        setPlayers()
+        
+        self.timer = Timer.scheduledTimer(withTimeInterval: 2,repeats: true , block: {_ in
+            self.drawCards();
+            self.roundWinner();
+            if (self.checkGameOver()) {
+                self.timer.invalidate();
+                self.gameOver();
+            }
+        })
+        
+
     }
     
+    override public var shouldAutorotate: Bool {
+       return false
+     }
+     override public var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+       return .landscapeRight
+     }
+     override public var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
+       return .landscapeRight
+     }
     
-    func startGame() {
-        while (!checkGameOver()) {
-            drawCards()
-            roundWinner()
-        }
-        gameOver()
-        
+    func setPlayers() {
+        panel_LBL_player1.text  = player1
+        panel_LBL_player2.text = player2
     }
     
     func drawCards() {
         panel_IMG_card1.image = images.randomElement()
         panel_IMG_card2.image = images.randomElement()
+        
+        round += 1
+        panel_LBL_round.text = "Round: \(self.round)"
     }
     
     func roundWinner() {
         var card1: Int = images.firstIndex(of:panel_IMG_card1.image!)!
         var card2: Int = images.firstIndex(of:panel_IMG_card2.image!)!
+    
         card1 = heroOrVillain(index: card1)
         card2 = heroOrVillain(index: card2)
         
         if (card1 > card2) {
             score1+=1
-            panel_LBL_score1.text = "\(score1)"
         }
         else if (card2 > card1) {
             score2+=1
-            panel_LBL_score2.text = "\(score2)"
         }
-    
-
+        else {
+        }
+        
+        panel_LBL_score1.text = "\(score1)"
+        panel_LBL_score2.text = "\(score2)"
     }
     
     func heroOrVillain(index: Int) -> Int {
@@ -74,7 +97,7 @@ class GameView: UIViewController {
     }
     
     func checkGameOver() -> Bool {
-        if (score1 >= 15 || score2 >= 15) {
+        if (score1 >= 5 || score2 >= 5) {
             return true
         }
         else {
@@ -92,6 +115,7 @@ class GameView: UIViewController {
         else {
             
         }
+        
     }
 
 }
